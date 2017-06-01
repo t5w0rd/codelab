c5a2311969dc045447247b020008be56c88e8b5c
jing.liu <jing.liu@yff.com>
2017-06-01 22:19:11 +0800

    multijobs

--- tutils.py
+++ tutils.py
@@ -1066,15 +1066,69 @@ def checkHideArgvs():
     os.remove(tmpfile)
     sys.argv = sys_argv
 
-def multijobs(target, args, workers=None):
+def multijobs(target, argslist, workers=None):
     if not workers:
         workers = multiprocessing.cpu_count()
-    jobs = len(args)
-    workers = min(jobs, workers)
-    pool = multiprocessing.Pool(processes=workers)
-    ret = pool.map(target, args)
-    pool.close()
-    pool.join()
+    workers = min(len(argslist), workers)
+    msgq = multiprocessing.Queue()
+
+    # target wapper
+    def worker(target, args):
+        res = target(*args)
+        pid = os.getpid()
+        msgq.put((pid, res))
+
+    # add proc to waiting
+    waiting = collections.deque()
+    for args in argslist:
+        args_wapper = [target, args]
+        proc = multiprocessing.Process(target=worker, args=args_wapper)
+        waiting.append(proc)
+
+    seq = []
+    running = set()
+    results = {}
+    dataNum = 1
+    while True:
+        # move proc from waiting to running
+        for i in xrange(max(0, min(len(waiting), workers - len(running)))):
+            proc = waiting.popleft()
+            running.add(proc)
+            proc.start()
+            seq.append(proc.pid)
+
+        if len(running) == 0:
+            break
+
+        # if len(running) > 0, wait for blocking msgq.get() instead of time.sleep()
+        for i in xrange(dataNum):
+            pid, res = msgq.get()
+            results[pid] = res
+        dataNum = 0
+
+        # check proc is alive or not
+        dead = []
+        for proc in running:
+            if not proc.is_alive():
+                dead.append(proc)
+                
+        # remove dead proc from running
+        for proc in dead:
+            running.remove(proc)
+            proc.join()
+            dataNum += 1
+
+    # put msgq data left to results map
+    while not msgq.empty():
+        pid, res = msgq.get()
+        results[pid] = res
+    msgq.close()
+
+    # collect retults of child proc
+    ret = []
+    for pid in seq:
+        ret.append(results[pid])
+
     return ret
 
 try:
@@ -1284,8 +1338,7 @@ try:
 except:
     pass
 
-def _sshExecWorker(args):
-    host, port, user, passwd, cmd = args
+def _sshExecWorker(host, port, user, passwd, cmd):
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host, port=port, username=user, password=passwd)
@@ -1444,10 +1497,10 @@ if __name__ == '__main__':
             assert(paramiko)
             assert(raddrs)
             assert(user and passwd)
-            args = []
+            argslist = []
             for host, port in raddrs:
-                args.append((host, port, user, passwd, cmd))
-            res = multijobs(_sshExecWorker, args, workers=len(args))
+                argslist.append((host, port, user, passwd, cmd))
+            res = multijobs(_sshExecWorker, argslist, workers=len(argslist))
             for out, err in res:
                 sys.stdout.write(out)
                 if err:
