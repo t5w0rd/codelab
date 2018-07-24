91f704d9fc58ed449b5af50e66757d940012b1d9
t5w0rd <lightning_0721@163.com>
2018-07-24 13:24:15 +0800

    getpath.sh

---
+++ getpath.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+getpath() {
+    # 获取脚本所在目录
+    SOURCE=$1
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
+    echo $DIR
+}
+
+echo $(getpath $1)
