d4dd2eece840f2cdea6d8270ea25bb2e2b82eeb5
BuildTools <unconfigured@null.spigotmc.org>
2019-01-03 21:13:31 +0800

    test_select

---
+++ test_select.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/times.h>
+
+int main() {
+    struct timeval tv;
+    struct tms clk;
+    for (;;) {
+        tv.tv_sec = 0;
+        tv.tv_usec = 1;
+        select(0, NULL, NULL, NULL, &tv);
+        times(&clk);
+    }
+    return 0;
+}
