fe54661871c2bf68ebd1c22e55fd29a96f492548
t5w0rd <lightning_0721@163.com>
2019-09-23 18:22:56 +0800

    增加FGO抓取脚本

---
+++ crawler4fgo.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+
+import requests
+from lxml import etree
+import os
+from collections.abc import Iterator
+
+
+wiki_url_base = r'https://fgo.wiki'
+
+
+def get_full_url(relative_path):
+    return wiki_url_base + relative_path
+
+
+def try_xpath(html, xpath_fmt, *args):
+    res = html.xpath(xpath_fmt.format(*args))
+    if len(res) is 0:
+        return None
+    return res[0]
+
+
+servant_page_url_fmt = get_full_url(r'/w/{name_link}')
+
+servant_card_names = ['初始状态', '灵基再临I', '灵基再临Ⅲ', '灵基再临Ⅳ', '愚人节']
+
+servant_card_page_xpath_fmt = r'//div[@title="{}"]/p/a/@href'
+
+
+def parser_function(data, *args):
+    """
+    解析器定义模板
+    yield next_url, next_parser, next_args
+    or
+    return next_url, next_parser, next_args
+    or
+    nothing to return
+    :param data:
+    :param args:
+    :return: next_url, next_parser, next_args
+    """
+    pass
+
+
+def parse_servants_svt(data):
+    """
+    解析英灵图鉴中的csv数据
+    :param data:
+    :return:
+    """
+    pass
+
+
+def load_csv_file(data, path):
+    """
+    加载csv文件
+    :param data:
+    :param path:
+    :return:
+    """
+    with open(path, 'r') as fp:
+        return None, parse_servants_data, (fp.read(),)
+
+
+def parse_servants_data(data, servants_data):
+    """
+    解析所有从者数据
+    :param data:
+    :param servants_data:
+    :return:
+    """
+    lns = servants_data.splitlines()
+    titles = lns[0].split(',')
+    for ln in lns[1:]:
+        fields = ln.split(',')
+        servant = {titles[i]: item for i, item in enumerate(fields)}
+        next_url = servant_page_url_fmt.format_map(servant)
+        yield next_url, parse_servant_page, (servant,)
+
+def parse_servant_page(data, servant):
+    """
+    解析从者页
+    :param data:
+    :param servant:
+    :return:
+    """
+    html = etree.HTML(data)
+    for card_name in servant_card_names:
+        href = try_xpath(html, servant_card_page_xpath_fmt, card_name)
+        if href is not None:
+            yield get_full_url(href), parse_servant_card_page, (servant, card_name)
+
+
+servant_card_image_xpath_fmt = r'//div[@class="fullImageLink"]/a/@href'
+
+
+def parse_servant_card_page(data, servant, card_name):
+    """
+    解析从者卡面页
+    :param data:
+    :param servant:
+    :param card_name:
+    :return:
+    """
+    html = etree.HTML(data)
+    href = try_xpath(html, servant_card_image_xpath_fmt)
+    if href is not None:
+        yield get_full_url(href), save_servant_card_image, (servant, card_name)
+
+
+save_base = r'crawler4fgo'
+
+
+def save_servant_card_image(data, servant, card_name):
+    """
+    保存从者图片
+    :param data:
+    :param servant:
+    :param card_name:
+    :return:
+    """
+    path = save_base + r'/{name_link}'.format_map(servant)
+    os.makedirs(path, mode=0o755, exist_ok=True)
+
+    file_name = r'{}/{}.png'.format(path, card_name)
+    print('保存[{}]'.format(file_name))
+    with open(file_name, 'wb') as fp:
+        fp.write(data)
+
+
+def invoke_parser(url, parser, *args):
+    def next_invoke(cur_result):
+        if isinstance(cur_result, tuple) and len(cur_result) == 3:
+            next_url, next_parser, next_args = cur_result
+            print('下轮析器进入队列[{}]'.format(next_parser.__name__))
+            # FIXME: enque
+            invoke_parser(next_url, next_parser, *next_args)
+        elif cur_result is None:
+            pass
+        else:
+            print('解析器返回结果格式错误[{}]'.format(parser.__name__))
+
+    data = None
+    if url is not None:
+        print('加载[{}]'.format(url))
+        with requests.get(url) as rsp:
+            data = rsp.content
+
+    print('调用解析器[{}]'.format(parser.__name__))
+    res = parser(data, *args)
+    if isinstance(res, Iterator):
+        for each in res:
+            next_invoke(each)
+    else:
+        next_invoke(res)
+
+
+if __name__ == '__main__':
+    invoke_parser(None, load_csv_file, 'fgo.txt')
